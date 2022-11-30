b00f83b87f239955cef4fc4fa1de95516d33795f
Maarten Raasveldt <mbraasveldt@gmail.com>
2022-11-30 23:12:02 +0100

    Project setup

---
+++ src/main/java/aoc/Day01.java
@@ -0,0 +1,7 @@
+package aoc;
+
+public class Day01 {
+    public void solve() {
+
+    }
+}
---
+++ src/main/java/aoc/Main.java
@@ -0,0 +1,7 @@
+package aoc;
+
+public class Main {
+    public static void main(String[] args) {
+        new Day01().solve();
+    }
+}
