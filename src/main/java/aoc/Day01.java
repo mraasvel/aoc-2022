831481ce166d4f1adb154f0b57487dfa1c9073d2
Maarten Raasveldt <mbraasveldt@gmail.com>
2022-12-01 10:03:12 +0100

    day 1: complexity improvement

--- src/main/java/aoc/Day01.java
+++ src/main/java/aoc/Day01.java
@@ -4,11 +4,25 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.PriorityQueue;
 import java.util.Scanner;
 
 public class Day01 {
-    public void solve(String filename) {
-        ArrayList<Integer> elves = new ArrayList<>();
+    ArrayList<Integer> elves = new ArrayList<>();
+
+    // Minheap for optimal space usage
+    // Stores at most MAX_HEAP_SIZE and contains the largest MAX_HEAP_SIZE elves
+    PriorityQueue<Integer> top_elves = new PriorityQueue<>();
+    static final int MAX_HEAP_SIZE = 3;
+
+    void addToTop(int elf) {
+        top_elves.add(elf);
+        if (top_elves.size() > MAX_HEAP_SIZE) {
+            top_elves.remove();
+        }
+    }
+
+    void parse(String filename) {
         try {
             int elf = 0;
             Scanner scanner = new Scanner(new File((filename)));
@@ -16,6 +30,7 @@ public class Day01 {
                 String line = scanner.nextLine();
                 if (line.length() == 0) {
                     elves.add(elf);
+                    addToTop(elf);
                     elf = 0;
                 } else {
                     elf += Integer.parseInt(line);
@@ -24,13 +39,35 @@ public class Day01 {
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
-        Collections.sort(elves);
-        Collections.reverse(elves);
-        System.out.printf("%d %d %d\n", elves.get(0), elves.get(1), elves.get(2));
-        System.out.printf("P2: %d\n", elves.get(0) + elves.get(1) + elves.get(2));
+    }
+    void solve() {
+        if (elves.isEmpty()) {
+            throw new RuntimeException();
+        }
+
+        // first solution attempt
+//        Collections.sort(elves);
+//        Collections.reverse(elves);
+//        int max = elves[0];
+//        int top3 = elves[0] + elves[1] + elves[2];
+
+        // functional stream stolen from Tijmen
+        int p1 = elves.stream().mapToInt(a -> a).max().getAsInt();
+        int p2 = elves.stream().sorted(Collections.reverseOrder()).limit(3).reduce(0, Integer::sum);
+
+        // PQ solution
+        // PQ contains at most 3 elements making it O(1) in terms of space complexity
+        assert top_elves.size() == 3;
+        assert p1 == top_elves.peek();
+        assert p2 == top_elves.stream().reduce(0, Integer::sum);
+
+        System.out.printf("P1: %d\n", p1);
+        System.out.printf("P2: %d\n", p2);
     }
 
     public static void main(String[] args) {
-        new Day01().solve("inputs/01.txt");
+        Day01 day01 = new Day01();
+        day01.parse("inputs/01.txt");
+        day01.solve();
     }
 }
